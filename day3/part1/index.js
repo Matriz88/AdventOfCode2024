58a6766bd24fb606b9d141348fda9caec60a359a
Matriz <matriz@hotmail.it>
2024-12-06 02:43:10 +0100

    day 3

---
+++ day3/part1/index.js
@@ -0,0 +1,24 @@
+const fs = require("node:fs");
+const path = require("node:path");
+fs.readFile(path.join(process.cwd(), "message.txt"), "utf8", (err, text) => {
+  if (err) {
+    return;
+  }
+
+  const regex = /mul\((\d+,\d+)\)/g;
+
+  const matches = [];
+  let match;
+
+  while ((match = regex.exec(text)) !== null) {
+    matches.push(match[1]);
+  }
+
+  let sum = 0;
+  matches.forEach((element) => {
+    const [a, b] = element.split(",");
+    sum += a * b;
+  });
+
+  console.log(sum);
+});
